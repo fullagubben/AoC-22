870896137e7dd489898995c312066f0ee3a5c414
fullagubben <84836806+fullagubben@users.noreply.github.com>
2022-12-04 13:11:27 +0100

    Update DAG3.py

--- DAG3.py
+++ DAG3.py
@@ -1,8 +1,6 @@
 with open('LIST3.txt', 'r') as f:
-    lines = f.readlines()
-    rucksacks = [entry.strip() for entry in lines]
-matches = []
-total = 0
+    rucksacks = [entry.strip() for entry in f.readlines()]
+matches = [] ;total = 0
 for content in rucksacks:    
     firstpart, secondpart= content[:len(content)//2], content[len(content)//2:]
     firstpartset = set(firstpart)
