9c94beccb9d8678a6e2d4fb8db5622c24ba490dd
fullagubben <84836806+fullagubben@users.noreply.github.com>
2022-12-09 00:16:02 +0100

    Added maps

--- DAG3/DAG3.py
+++ DAG3/DAG3.py
@@ -1,8 +1,8 @@
 with open('LIST3.txt', 'r') as f:
     rucksacks = [entry.strip() for entry in f.readlines()]
-matches = [] ;total = 0
+matches = [] ; total = 0
 for content in rucksacks:    
-    firstpart, secondpart= content[:len(content)//2], content[len(content)//2:]
+    firstpart, secondpart = content[:len(content)//2], content[len(content)//2:]
     firstpartset = set(firstpart)
     secondpartset = set(secondpart)
     if set(firstpart) & set(secondpart):
